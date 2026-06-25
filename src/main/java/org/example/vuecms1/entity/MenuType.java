46f443e8fa2f3d641f1b040586c41b9909545926
knee <279003147@qq.com>
2026-06-25 21:20:32 +0800

    初代1

--- src/main/java/org/example/vuecms1/entity/MenuType.java
+++ src/main/java/org/example/vuecms1/entity/MenuType.java
@@ -1,17 +1,17 @@
-//package org.example.vuecms1.entity;
-//
-//public enum MenuType {
-//    DIRECTORY("目录"),
-//    MENU("菜单"),
-//    BUTTON("按钮");
-//
-//    private String value;
-//
-//    MenuType(String value) {
-//        this.value = value;
-//    }
-//
-//    public String getValue() {
-//        return value;
-//    }
-//}
+package org.example.vuecms1.entity;
+
+public enum MenuType {
+    DIRECTORY("目录"),
+    MENU("菜单"),
+    BUTTON("按钮");
+
+    private String value;
+
+    MenuType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
